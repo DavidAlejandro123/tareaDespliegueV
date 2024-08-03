020a84fb9a93de0c00dceb1acb4a8ec898bf3bfb
David <alejandroariste210@gmail.com>
2024-08-03 14:57:24 -0400

    archivo vercel3

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'blog.apps.BlogConfig',
-    'blog'
+    'blog.apps.BlogConfig',
+    #'blog'
 ]
 
 #FALTA AGREGAR NOMBRE DE APP A INSTALLED_APP
